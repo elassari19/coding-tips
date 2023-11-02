5dda176346885ba4b372428889943467b554db79
Hicham El Assari <elassari19@gmail.com>
2023-11-02 23:27:51 +0100

    Update UseMome.tsx

--- HOC/UseMome.tsx
+++ HOC/UseMome.tsx
@@ -1,1 +1,20 @@
+"use client"
 
+import { useMemo } from "react"
+
+interface Props extends React.HtmlHTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode,
+  dependencies: any[]
+}
+
+const UseMemo = ({ children, dependencies }: Props) => {
+  return (
+    <>
+      {
+        useMemo(() => children, dependencies)
+      }
+    </>
+  )
+}
+
+export default UseMemo
