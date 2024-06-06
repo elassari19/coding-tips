38ba58cca429a1c57bac271de05925bfeb36cbb8
Hicham El Assari <elassari19@gmail.com>
2024-06-06 23:30:46 +0100

    Create useAnimatedCounter.tsx

---
+++ docs/hooks/useAnimatedCounter.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import { animate } from "framer-motion";
+
+const useAnimatedCounter = (
+  maxValue: number,
+  initialValue = 0,
+  duration = 1,
+) => {
+  const [counter, setCounter] = useState<any>(initialValue);
+
+  useEffect(() => {
+    const controls = animate(initialValue, maxValue, {
+      duration,
+      onUpdate(value) {
+        setCounter(value.toFixed(0));
+      }
+    });
+    return () => controls.stop();
+  }, [initialValue, maxValue, duration]);
+
+  return counter;
+}
